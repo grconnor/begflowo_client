a7a29955c15b1116acfc69083f21ddb18e0f8ef6
Connor Roelofsen <connorroelofsen15@gmail.com>
2021-05-26 14:00:52 +0200

    Styling and scaffolds UserForm component.

---
+++ src/components/UserForm.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+export class UserForm extends Component {
+  state = {
+    step: 1,
+    regnum: "",
+    ordernum: "",
+    transmission: "",
+    make: "nissan",
+    model: "",
+    year: "",
+    color: "",
+    mileage: "",
+  }
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default UserForm
